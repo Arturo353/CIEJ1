f80678c4bdca1c0fadeb329b83f0a064a07e3043
Arturo353 <sick.strigoi@live.com>
2023-09-25 15:49:43 -0600

    java file upload

---
+++ C1Ej1Java.java
@@ -0,0 +1,22 @@
+
+ */
+package c1ej1java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Arturo
+ */
+public class C1Ej1Java {
+
+   
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("A commit has been made");
+       
+        
+        
+    }
+    
+}
